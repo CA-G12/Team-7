04f1df67420a4d45af0da51da2e8b68725d643c2
shamskhodary <shams.khodary@gmail.com>
2022-07-21 17:17:26 +0300

    Adding the js for the question page relates #5

---
+++ Data.js
@@ -0,0 +1,45 @@
+const questions = [
+    {
+      question: 'How do you call a function named "myFunction"?',
+      a: "myFunction()",
+      b: "call function myFunction()",
+      c: "call myFunction()",
+      d: "None of the mentioned",
+      correct: "a",
+    },
+    {
+      question: "How can a datatype be declared to be a constant type?",
+      a: "const",
+      b: "var",
+      c: "let",
+      d: "const",
+      correct: "a",
+    },
+    {
+      question: "When an operator’s value is NULL, the typeof returned by the unary operator is",
+      a: "Integer",
+      b: "Undefined",
+      c: "Object",
+      d: "Boolean",
+      correct: "d",
+    },
+    {
+      question: 'What does the Javascript “debugger” statement do?',
+      a: "It will debug all the errors in the program at a runtime",
+      b: "it acts as a breakpoint in a program",
+      c: "it will debug error in the current statement",
+      d: "All of the above",
+      correct: "b",
+    },
+    {
+      question: 'What does the ‘toLocateString()’ method do in JS?',
+      a: "Returns a localized object representation",
+      b: "Returns a localized string representation of an object",
+      c: "Returns a parsed string",
+      d: "None of the above",
+      correct: "b",
+    }
+  ];
+
+
+  export {questions};
